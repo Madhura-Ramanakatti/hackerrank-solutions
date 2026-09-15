a6734d8c11e13111675a4c7a5af0fc0d4682354e
MADHURA MALLAPPA RAMANAKATTI <madhuraramanakatti@gmail.com>
2026-09-15 18:16:20 +0530

    ✅ Python: Division [python3] — approach1

---
+++ solutions/python3/easy/python-division/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Problem     Python: Division
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-15, 06:16 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+    
+    
